efdea011b4844dc3a779c450d4ffda32ba8e54e2
Attila Bagossy <bagossyattila@outlook.com>
2018-10-23 17:13:25 +0200

    Added the isSorted method to ListUtils

--- src/main/java/com/codeosseum/eligo/util/ListUtils.java
+++ src/main/java/com/codeosseum/eligo/util/ListUtils.java
@@ -11,6 +11,16 @@ public final class ListUtils {
         return set.size() != list.size();
     }
 
+    public static <E extends Comparable<E>> boolean isSorted(final List<E> list) {
+        for (int i = 0; i < list.size() - 1; ++i) {
+            if (list.get(i).compareTo(list.get(i + 1)) > 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     private ListUtils() {
         // Cannot be constructed.
     }
